9aa767cf1a91ff29d70915842d931b7c066af0b6
Jason McCarrell <jason.abz@gmail.com>
2018-05-29 16:10:49 -0400

    added test for exanable. Mocha working.

---
+++ expandable.spec.js
@@ -0,0 +1,45 @@
+import chai from 'chai';
+
+import expandable from './expandable.js';
+
+var assert = chai.assert;
+
+describe('expandable', () => {
+    const person = {
+        firstName: 'Jason',
+        company: {
+            id: 1,
+            name: 'Quaker Oats',
+        },
+    }
+    const keyToObject = 'company';
+    const keyToScalar = 'firstName';
+    const keyNotExist = 'flavour';
+    const sampleOneToOne = {
+        Person: ['company'],
+    };
+
+    describe('with oneToOne not set', () => {
+        it('should return true with a model key pointing to an object', () => {
+            assert.isTrue(expandable(person, keyToObject))
+        });
+
+        it('should return false with a key pointing to a scalar', () => {
+            assert.isNotTrue(expandable(person, keyToScalar))
+        });
+
+        it('should return false with a key not found in the model', () => {
+            assert.isNotTrue(expandable(person, keyNotExist))
+        });
+    });
+
+    describe('with oneToOne set', () => {
+        it('should return true with key found in the model, but without the modelName or key found in the oneToOne arg', () => {
+            assert.isTrue(expandable(person, keyToObject))
+        });
+
+        it('should return false with key found in the model and modelName and key found in the oneToOne arg', () => {
+            assert.isNotTrue(expandable(person, keyToObject, 'Person', sampleOneToOne))
+        });
+    });
+});
